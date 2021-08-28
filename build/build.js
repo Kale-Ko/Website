6e0e7fb4c01649e53483da1d8321fe33c47543fd
Kale-Ko <github@kaleko.dev>
2021-08-28 14:16:43 -0400

    Update

--- build/build.js
+++ build/build.js
@@ -1,18 +1,16 @@
 const fs = require("fs")
 const placeholders = require("./placeholders.js")
 
-console.log("Started Building")
+console.log("Started building pages")
+
+console.log(placeholders)
 
 var pages = fs.readdirSync("./")
-
-console.log(pages)
 
 pages.forEach(page => {
     if (!page.endsWith(".html")) return
 
     var content = fs.readFileSync("./" + page).toString()
-
-    console.log(page)
 
     for (var key in Object.keys(placeholders)) {
         console.log("{" + key + "}", "g")
