9687c2d61b9a49e623ac60de96ecba089cd7ffca
Kale-Ko <github@kaleko.dev>
2021-08-28 14:07:15 -0400

    Update

--- build/build.js
+++ build/build.js
@@ -3,12 +3,12 @@ const placeholders = require("./placeholders.js")
 
 var pages = fs.readdirSync("../")
 pages.forEach(page => {
-    if (!page.endsWith(".html")) continue
+    if (!page.endsWith(".html")) return
 
     var content = fs.readFileSync("../" + page).toString()
 
     for (var key in Object.keys(placeholders)) {
-        content = content.replace(new RegExp("{" + key + "}", g), placeholders[key])
+        content = content.replace(new RegExp("{" + key + "}", "g"), placeholders[key])
     }
 
     fs.writeFileSync("../" + page, content)
