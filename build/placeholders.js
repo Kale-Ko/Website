29e37221fe1c45f403126b140f4c2d84586b5bf7
Kale-Ko <github@kaleko.ga>
2021-08-28 14:33:10 -0400

    Update

--- build/placeholders.js
+++ build/placeholders.js
@@ -1,5 +1,14 @@
 module.exports = {
     placeholders: {
-        meta: '\n    <link ref="icon" href="/assets/icon.png">\n    <link rel="stylesheet" href="/styles/global.css">\n    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n\n    <meta name="description" content="My new and improved website">\n    <meta name="keywords" content="Kale, Ko, Kale-Ko, Coding, Github, Games, Projects">\n    <meta name="author" content="Kale Ko (https://kaleko.ga)">\n    <link rel="author" href="https://kaleko.ga/">\n    <link rel="license" href="/LICENSE">\n    <link rel="manifest" href="/manifest.json">'
+        meta: `    <link ref="icon" href="/assets/icon.png">
+    <link rel="stylesheet" href="/styles/global.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description" content="My new and improved website">
+    <meta name="keywords" content="Kale, Ko, Kale-Ko, Coding, Github, Games, Projects">
+    <meta name="author" content="Kale Ko (https://kaleko.ga)">
+    <link rel="author" href="https://kaleko.ga/">
+    <link rel="license" href="/LICENSE">
+    <link rel="manifest" href="/manifest.json">`
     }
 }
