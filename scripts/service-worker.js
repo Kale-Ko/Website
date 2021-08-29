20425bfc5ee629ed81ba289ea9d9686a1cb7e11f
Kale Ko <orangekalekid@bluetreetop.com>
2021-08-29 21:01:43 +0000

    Update

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -21,3 +21,30 @@ self.addEventListener("fetch", event => {
         )
     }
 })
+
+/*
+self.addEventListener("push", event => {
+    var data = event.data.json()
+
+    var payload = {
+        body: data.body || "",
+        data: {
+            url: data.url || ""
+        }
+    }
+
+    event.waitUntil(self.registration.showNotification("Push", payload))
+})
+
+self.addEventListener("notificationclick", event => {
+    var notification = event.notification
+
+    if (event.action == "close") {
+        notification.close()
+    } else if (notification.data.url != "") {
+        clients.openWindow(notification.data.url)
+
+        notification.close()
+    }
+})
+*/
