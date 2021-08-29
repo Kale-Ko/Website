b1768f2420386ac5c20e2cc869d04e23145d4087
Kale-Ko <github@kaleko.dev>
2021-08-29 21:13:57 +0000

    Update

--- build/builddata.js
+++ build/builddata.js
@@ -16,22 +16,19 @@ module.exports = {
     <meta name="theme-color" content="#ffbb00">`,
 
         offlinemeta: `<link rel="icon" href="/assets/icon-grey@64.png">
-    <link rel="apple-touch-icon" href="/assets/icon@64.png">
-    <link rel="stylesheet" href="/styles/global.css">
+    <link rel="apple-touch-icon" href="/assets/icon-grey@64.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <meta name="description" content="Error 404 or site offline">
+    <meta name="description" content="Site offline">
     <meta name="keywords" content="Kale, Ko, Kale-Ko, Error, Offline">
     <meta name="robots" content="noindex">
     <meta name="author" content="Kale Ko (https://kaleko.ga)">
     <meta name="creator" content="Kale Ko (https://kaleko.ga)">
     <link rel="author" href="https://kaleko.ga/">
     <link rel="license" href="/LICENSE">
-    <link rel="manifest" href="/assets/manfiest.json">
     <meta name="theme-color" content="#ffbb00">`,
 
-        global: `<script src="/scripts/Vue.js"></script>
-    <script src="/scripts/global.js"></script>`
+        global: `<script src="/scripts/global.js"></script>`
     },
     replacements: [
         { from: "<title>", to: "<title>Kale Ko - " }
