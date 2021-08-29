b5b04d3fbe38eb0f1cf4b6e3956547d0b6bb636c
Kale-Ko <github@kaleko.dev>
2021-08-29 19:47:13 +0000

    Update

--- build/builddata.js
+++ build/builddata.js
@@ -1,25 +1,34 @@
 module.exports = {
     placeholders: {
         meta: `<link rel="icon" href="/assets/icon@64.png">
+    <link rel="apple-touch-icon" href="/assets/icon@64.png">
     <link rel="stylesheet" href="/styles/global.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <meta name="description" content="My new and improved website">
     <meta name="keywords" content="Kale, Ko, Kale-Ko, Coding, Github, Games, Projects">
+    <meta name="robots" content="all">
     <meta name="author" content="Kale Ko (https://kaleko.ga)">
+    <meta name="creator" content="Kale Ko (https://kaleko.ga)">
     <link rel="author" href="https://kaleko.ga/">
     <link rel="license" href="/LICENSE">
-    <link rel="manifest" href="/assets/manfiest.json">`,
+    <link rel="manifest" href="/assets/manfiest.json">
+    <meta name="theme-color" content="#ffbb00">`,
 
         offlinemeta: `<link rel="icon" href="/assets/icon-grey@64.png">
+    <link rel="apple-touch-icon" href="/assets/icon@64.png">
     <link rel="stylesheet" href="/styles/global.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <meta name="description" content="Error 404 or site offline">
     <meta name="keywords" content="Kale, Ko, Kale-Ko, Error, Offline">
+    <meta name="robots" content="noindex">
     <meta name="author" content="Kale Ko (https://kaleko.ga)">
+    <meta name="creator" content="Kale Ko (https://kaleko.ga)">
     <link rel="author" href="https://kaleko.ga/">
-    <link rel="license" href="/LICENSE">`,
+    <link rel="license" href="/LICENSE">
+    <link rel="manifest" href="/assets/manfiest.json">
+    <meta name="theme-color" content="#ffbb00">`,
 
         global: `<script src="/scripts/Vue.js"></script>
     <script src="/scripts/global.js"></script>`
--- scripts/global.js
+++ scripts/global.js
@@ -8,3 +8,15 @@ const globalapp = new Vue({
 })
 
 if ("serviceWorker" in navigator) navigator.serviceWorker.register("/service-worker.js")
+
+var settings = [
+    { id: "darkmode", title: "Darkmode", type: "checkbox", default: false }
+]
+
+function updateSettings() {
+    settings.forEach(setting => { if (localStorage.getItem(setting.id) == null) localStorage.setItem(setting.id, setting.default) })
+
+    if (localStorage.getItem("darkmode") == "true") document.querySelector("html").setAttribute("class", "darkmode")
+    else document.querySelector("html").setAttribute("class", "")
+}
+updateSettings()
