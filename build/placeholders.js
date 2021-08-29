ae2d8755ccfad15c3bf163f504b3faf6d492a23a
Kale-Ko <github@kaleko.ga>
2021-08-29 11:24:14 -0400

    Update

--- build/placeholders.js
+++ build/placeholders.js
@@ -1,5 +1,5 @@
 module.exports = {
-    meta: `<link rel="icon" href="/assets/icon.png">
+    meta: `<link rel="icon" href="/assets/icon@64.png">
     <link rel="stylesheet" href="/styles/global.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -9,7 +9,7 @@ module.exports = {
     <link rel="author" href="https://kaleko.ga/">
     <link rel="license" href="/LICENSE">`,
 
-    offlinemeta: `<link rel="icon" href="/assets/icon-grey.png">
+    offlinemeta: `<link rel="icon" href="/assets/icon-grey@64.png">
     <link rel="stylesheet" href="/styles/global.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
