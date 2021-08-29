c902a530f1382183e8a3adbd0e266a8a42a3ffc2
Kale Ko <orangekalekid@bluetreetop.com>
2021-08-29 20:17:16 +0000

    Update

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -6,22 +6,16 @@ self.addEventListener("install", event => {
     self.skipWaiting()
 })
 
-self.addEventListener("activate", (event) => {
-    event.waitUntil((async () => { if ("navigationPreload" in self.registration) await self.registration.navigationPreload.enable() })())
-
-    self.clients.claim()
-})
+self.addEventListener("activate", event => { self.clients.claim() })
 
 self.addEventListener("fetch", event => {
     if (event.request.mode == "navigate") {
         event.respondWith(
             (async () => {
                 try {
-                    var data = await fetch(event.request).then(res => { if (res.status > 400) { throw new Error("Offline") } else return res })
-
-                    return data
+                    return await fetch(event.request).then(res => { if (res.status > 400) { throw new Error("Offline") } else return res })
                 } catch (error) {
-                    return caches.open("offline").then(cache => { return cache.match(req.url) || cache.match("/offline/") })
+                    return await caches.open("offline").then(async cache => { return await cache.match(event.request.url) || await cache.match("/offline/") })
                 }
             })()
         )
