a7c674824ff27ab6570208528996c1288020dc56
Kale-Ko <github@kaleko.dev>
2021-08-30 00:13:11 +0000

    Update

--- build/localtest.js
+++ build/localtest.js
@@ -1,6 +1,7 @@
 const fs = require("fs")
 const { exec } = require("child_process")
 const http = require("http")
+const WebSocketServer = require('websocket').server
 
 function build() {
     console.log("Cloning files")
@@ -22,16 +23,12 @@ function build() {
     }
     scan("./", "/")
 
-    console.log("Finished cloning files")
-
     console.log("Building pages")
 
     exec("cd ./test/ && node build/build.js", (err, stdout, stderr) => {
         if (stdout && stdout.toString() != "Started building pages\n") console.log(stdout)
         if (stderr) console.log(stderr)
     })
-
-    console.log("Finished building pages")
 }
 build()
 
@@ -64,14 +61,26 @@ const server = http.createServer((req, res) => {
         res.statusMessage = "Ok"
         Object.keys(typeMappings).forEach(key => { if (req.url.endsWith("." + key)) res.setHeader("Content-Type", typeMappings[key]) })
 
-        res.end(fs.readFileSync("./test" + req.url))
+        if (res.getHeader("Content-Type") == typeMappings["html"]) {
+            res.end(fs.readFileSync("./test" + req.url).toString().replace("</body>", `    <script>
+		var socket = new WebSocket(window.location.protocol.replace("http", "ws") + "//" + window.location.host + "/livereload")
+		socket.onmessage = (msg) => { if (msg.data == "reload") window.location.reload() }
+    </script>
+</body>`))
+        } else res.end(fs.readFileSync("./test" + req.url))
     } else {
         if (fs.existsSync("./test" + req.url + "/index.html")) {
             res.statusCode = 200
             res.statusMessage = "Ok"
             res.setHeader("Content-Type", typeMappings["html"])
 
-            res.end(fs.readFileSync("./test" + req.url + "/index.html"))
+            if (res.getHeader("Content-Type") == typeMappings["html"]) {
+                res.end(fs.readFileSync("./test" + req.url + "/index.html").toString().replace("</body>", `    <script>
+        var socket = new WebSocket(window.location.protocol.replace("http", "ws") + "//" + window.location.host + "/livereload")
+        socket.onmessage = (msg) => { if (msg.data == "reload") window.location.reload() }
+    </script>
+</body>`))
+            } else res.end(fs.readFileSync("./test" + req.url + "/index.html"))
         } else {
             res.statusCode = 404
             res.statusMessage = "Not found"
@@ -84,20 +93,39 @@ const server = http.createServer((req, res) => {
 
 server.listen(8000, () => { console.log("Http server started on 8000") })
 
+const wsServer = new WebSocketServer({ httpServer: server })
+
+var connections = []
+
+wsServer.on("request", request => {
+    var connection = request.accept()
+
+    connections.push(connection)
+    connection.on("close", () => { connections.splice(connections.indexOf(connection), 1) })
+})
+
 console.log("Watching files")
 
-function scan(dir, name) {
+function scan(dir, name, serverfile) {
     var files = fs.readdirSync(dir)
 
     files.forEach(file => {
         if (file == ".git" || file == ".gitignore" || file == "package-lock.json" || file == "node_modules" || file == "test") return
 
         if (fs.statSync(dir + file).isDirectory()) {
-            scan(dir + file + "/", name + "/" + file)
+            scan(dir + file + "/", name + "/" + file, file == "build")
         } else fs.watchFile(dir + file, () => {
-            console.log("Changes detected, rebuilding..")
+            if (serverfile) {
+                console.log("Changes detected, restarting..")
 
-            build()
+                process.exit(0)
+            } else {
+                console.log("Changes detected, rebuilding..")
+
+                build()
+
+                connections.forEach(connection => { connection.send("reload") })
+            }
         })
     })
 }
