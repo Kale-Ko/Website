24b07f479b2f2a1b52bf6d926c3b4d114b05d160
Kale-Ko <github@kaleko.dev>
2021-09-28 17:12:16 -0400

    Update

--- build/build.js
+++ build/build.js
@@ -29,5 +29,44 @@ exec("npm i trash-cli -g").on("exit", code => {
 
     builddata.moves.forEach(move => { fs.renameSync(move.from, move.to) })
 
-    exec("trash pages/** LICENSE build/**")
+    exec("npm i html-minifier uglify-js css-minify minify-xml").on("exit", code => {
+        const minify_html = require('html-minifier').minify
+        const minify_js = require("uglify-js").minify
+        const minify_css = require("css-minify")
+        const minify_xml = require("minify-xml").minify
+
+        function scan(dir, name) {
+            var files = fs.readdirSync(dir)
+
+            files.forEach(async file => {
+                if (file == "pages" || file == "style" || file == "LICENSE" || file == "build" || file == "package-lock.json" || file == "node_modules") return
+
+                if (fs.statSync(dir + file).isDirectory()) {
+                    if (file == "fonts") return
+
+                    scan(dir + file + "/", name + "/" + file)
+                }
+                else {
+                    var contents = fs.readFileSync(dir + file).toString()
+
+                    if (file.endsWith(".html")) {
+                        contents = minify_html(contents, {
+                            collapseBooleanAttributes: true, collapseInlineTagWhitespace: true, collapseWhitespace: true, quoteCharacter: '"', removeAttributeQuotes: true, removeComments: true,
+                            minifyJS: text => { return minify_js(text).code }, minifyCSS: true
+                        });
+                    }
+                    else if (file.endsWith(".js")) contents = minify_js(contents).code
+                    else if (file.endsWith(".css")) contents = await minify_css(contents)
+                    else if (file.endsWith(".xml")) contents = minify_xml(contents)
+                    else if (file.endsWith(".json")) contents = JSON.stringify(JSON.parse(contents))
+                    else return
+
+                    fs.writeFileSync(dir + file, contents)
+                }
+            })
+        }
+        scan("./", "/")
+
+        exec("trash pages/** style/** LICENSE package.json package-lock.json node_modules/** build/**")
+    })
 })
