e8b8e2ba9a2fc2b227ae562dd8ca09ab1a0962f4
Kale-Ko <github@kaleko.dev>
2021-09-28 17:12:16 -0400

    Update

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,6 +1,9 @@
 self.addEventListener("install", event => {
     event.waitUntil(caches.open("offline").then(cache => {
-        return cache.addAll(["/offline/", "/assets/icon-grey@64.png"])
+        return cache.addAll([
+            "/offline/",
+            "/assets/icon-grey@64.png"
+        ])
     }))
 
     self.skipWaiting()
