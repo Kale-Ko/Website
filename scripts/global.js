c9cfdc30e91d5a71e7dc52687de2c015a7073b7d
Kale-Ko <github@kaleko.ga>
2021-10-25 15:49:58 -0400

    Update

--- scripts/global.js
+++ scripts/global.js
@@ -2,6 +2,7 @@ if ("serviceWorker" in navigator) navigator.serviceWorker.register("/service-wor
 
 var settings = [
     { id: "darkmode", title: "Darkmode", type: "checkbox", default: false },
+    { id: "fade-in", title: "Fade In", type: "checkbox", default: true },
     { id: "autoredirect", title: "Auto Redirect", type: "checkbox", default: false }
 ]
 
@@ -10,7 +11,9 @@ window.dispatchEvent(new CustomEvent("settingsready"))
 function updateSettings() {
     settings.forEach(setting => { if (localStorage.getItem(setting.id) == null) localStorage.setItem(setting.id, setting.default) })
 
-    if (localStorage.getItem("darkmode") == "true") document.querySelector("html").setAttribute("class", "darkmode")
-    else document.querySelector("html").setAttribute("class", "")
+    document.querySelector("html").setAttribute("class", "")
+
+    if (localStorage.getItem("darkmode") == "true") document.querySelector("html").classList.add("darkmode")
+    if (localStorage.getItem("fade-in") == "true") document.querySelector("html").classList.add("fade-in")
 }
 updateSettings()
