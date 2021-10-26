6b3748a449b527330556594459bc4665a6004f7a
Kale-Ko <github@kaleko.ga>
2021-10-26 14:54:13 -0400

    Update

--- build/build.js
+++ build/build.js
@@ -8,7 +8,7 @@ else {
     console.log("Installing dependencies")
 
     exec("npm i trash-cli -g").on("exit", code => {
-        exec("npm i html-minifier uglify-js css-minify minify-xml").on("exit", code => {
+        exec("npm i html-minifier uglify-js clean-css minify-xml").on("exit", code => {
             console.log("Finished installing dependencies")
 
             next()
@@ -28,9 +28,16 @@ function next() {
 
         Object.keys(builddata.placeholders).forEach(key => { content = content.replace(new RegExp("{" + key + "}", "g"), builddata.placeholders[key]) })
         builddata.replacements.forEach(replacement => { content = content.replace(replacement.from, replacement.to) })
-        var start = content.indexOf("{style=")
-        var end = content.indexOf("}", content.indexOf("{style=")) + 1
-        content = content.replace(content.substring(start, end), "<style>\n        " + fs.readFileSync("./styles/" + content.substring(start + 7, end - 1) + ".css").toString().replace(/\n/g, "\n         ") + "\n    </style>")
+        while (content.includes("{script=")) {
+            var start = content.indexOf("{script=")
+            var end = content.indexOf("}", content.indexOf("{script=")) + 1
+            content = content.replace(content.substring(start, end), "<script>\n        " + fs.readFileSync("./scripts/" + content.substring(start + 8, end - 1) + ".js").toString().replace(/\n/g, "\n         ") + "\n    </script>")
+        }
+        while (content.includes("{style=")) {
+            var start = content.indexOf("{style=")
+            var end = content.indexOf("}", content.indexOf("{style=")) + 1
+            content = content.replace(content.substring(start, end), "<style>\n        " + fs.readFileSync("./styles/" + content.substring(start + 7, end - 1) + ".css").toString().replace(/\n/g, "\n         ") + "\n    </style>")
+        }
 
         if (page == "index.html" || page == "404.html") fs.writeFileSync("./" + page, content)
         else {
@@ -44,17 +51,17 @@ function next() {
 
     const minify_html = require("html-minifier").minify
     const minify_js = require("uglify-js").minify
-    const minify_css = require("css-minify")
+    const minify_css = require("clean-css")
     const minify_xml = require("minify-xml").minify
 
     function scan(dir, name) {
         var files = fs.readdirSync(dir)
 
-        files.forEach(async file => {
-            if (file == "pages" || file == "style" || file == "LICENSE" || file == "build" || file == "package-lock.json" || file == "node_modules") return
+        files.forEach(file => {
+            if (file == "pages" || file == "styles" || file == "LICENSE" || file == "build" || file == "package-lock.json" || file == "node_modules") return
 
             if (fs.statSync(dir + file).isDirectory()) {
-                if (file == "fonts") return
+                if (dir == "fonts") return
 
                 scan(dir + file + "/", name + "/" + file)
             }
@@ -64,11 +71,12 @@ function next() {
                 if (file.endsWith(".html")) {
                     contents = minify_html(contents, {
                         collapseBooleanAttributes: true, collapseInlineTagWhitespace: true, collapseWhitespace: true, quoteCharacter: '"', removeAttributeQuotes: true, removeComments: true,
-                        minifyJS: text => { return minify_js(text).code }, minifyCSS: true
+                        minifyJS: text => minify_js(text).code,
+                        minifyCSS: text => new minify_css({ level: { 2: { all: true, roundingPrecision: false, removeUnusedAtRules: false } }, inline: ["local"] }).minify(text).styles
                     });
                 }
                 else if (file.endsWith(".js")) contents = minify_js(contents).code
-                else if (file.endsWith(".css")) contents = await minify_css(contents)
+                else if (file.endsWith(".css")) contents = new minify_css({ level: { 2: { all: true, roundingPrecision: false, removeUnusedAtRules: false } }, inline: ["local"] }).minify(contents).styles
                 else if (file.endsWith(".xml")) contents = minify_xml(contents)
                 else if (file.endsWith(".json")) contents = JSON.stringify(JSON.parse(contents))
                 else return
