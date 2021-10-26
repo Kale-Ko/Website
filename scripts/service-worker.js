af8e80379d0b66ae7cc5e7832a264c1a685e3fd0
Kale-Ko <github@kaleko.dev>
2021-10-26 15:59:41 -0400

    Update

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -10,20 +10,33 @@ self.addEventListener("fetch", event => {
             ])
         }))
 
-        event.respondWith(
             (async () => {
                 try {
-                    return await fetch(event.request).then(res => { if (res.status >= 500) { throw new Error("Offline") } else return res })
+                    return await fetch("https://api.kaleko.ga/v2/online/").then(res => { if (res.status != 200) { throw new Error("Offline") } })
                 } catch (error) {
-                    return caches.open("offline").then(async cache => { return await cache.match(event.request.url) || await cache.match("/offline/") })
+                    event.respondWith(caches.open("offline").then(async cache => { return await cache.match(event.request.url) || await cache.match("/offline/") }))
                 }
             })()
-        )
     } else {
         if (event.request.url.endsWith(".woff") || event.request.url.endsWith(".woff2")) {
             event.respondWith(
                 (async () => {
-                    var cache = await caches.open("font")
+                    var cache = await caches.open("fonts")
+
+                    var cacheres = await cache.match(event.request)
+
+                    if (cacheres == undefined) {
+                        await cache.add(event.request)
+
+                        return await cache.match(event.request)
+                    }
+                    else return cacheres
+                })()
+            )
+        } else if (event.request.url.endsWith(".png") || event.request.url.endsWith(".jpg") || event.request.url.endsWith(".jpeg") || event.request.url.endsWith(".ico")) {
+            event.respondWith(
+                (async () => {
+                    var cache = await caches.open("images")
 
                     var cacheres = await cache.match(event.request)
 
