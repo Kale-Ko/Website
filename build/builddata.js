9d2d8d1985cb721272cbdfbbe4c88f280d9ff813
Kale-Ko <github@kaleko.dev>
2021-11-06 18:35:33 -0400

    Update

--- build/builddata.js
+++ build/builddata.js
@@ -50,6 +50,7 @@ module.exports = {
     moves: [
         { from: "./scripts/service-worker.js", to: "./service-worker.js" },
         { from: "./assets/robots.txt", to: "./robots.txt" },
-        { from: "./assets/sitemap.xml", to: "./sitemap.xml" }
+        { from: "./assets/sitemap.xml", to: "./sitemap.xml" },
+        { from: "./LICENSE", to: "./license.txt" }
     ]
 }
