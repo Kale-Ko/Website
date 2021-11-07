55a1d19cd002e70979d34f9dc586f797e9b13849
Kale-Ko <github@kaleko.dev>
2021-11-06 21:23:46 -0400

    Update

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,3 +1,10 @@
+/**
+    @license
+    MIT License
+    Copyright (c) 2021 Kale Ko
+    See https://kaleko.ga/license.txt
+*/
+
 self.addEventListener("install", event => { self.skipWaiting() })
 self.addEventListener("activate", event => {
     event.waitUntil(caches.open("offline").then(cache => {
