e896c3117a838cb89f8b4a00607b13a54264bbae
Kale-Ko <github@kaleko.dev>
2021-11-20 14:20:17 -0500

    Update

--- scripts/backgroundfixer.js
+++ scripts/backgroundfixer.js
@@ -1,1 +1,1 @@
-if (document.querySelector("html").offsetHeight < window.innerHeight) document.querySelector("html").style.height = "1vh"
+if (document.querySelector("html").offsetHeight < window.innerHeight) document.querySelector("html").style.height = "100vh"
