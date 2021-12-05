92bd21456c801cd363407f8c2e7d523d3f097fc9
Kale-Ko <github@kaleko.dev>
2021-12-04 19:46:09 -0500

    Update

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -9,5 +9,25 @@ var navbar = document.querySelector("nav")
 var menu = navbar.querySelector("div.dropdown")
 
 menu.addEventListener("click", () => {
+    if (navbar.children.length > 1) {
+        for (var index = 0; navbar.children.length > 1; index++) {
+            var child = navbar.children.item(1)
 
+            if (!child.classList.contains("dropdown")) {
+                child.classList.add("visible")
+
+                menu.appendChild(child)
+            }
+        }
+    } else {
+        for (var index = 0; menu.children.length > 1; index++) {
+            var child = menu.children.item(1)
+
+            if (child.classList.contains("visible")) {
+                child.classList.remove("visible")
+
+                navbar.appendChild(child)
+            }
+        }
+    }
 })
