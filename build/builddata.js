2aa8f3348562830a37ed72c87584595358cace5f
Kale-Ko <github@kaleko.dev>
2022-09-20 19:45:46 -0400

    Fix a bunch of seo and formatting issues and use webp instead of png

--- build/builddata.js
+++ build/builddata.js
@@ -1,14 +1,17 @@
 module.exports = {
+    baseUrl: "https://kaleko.ga",
     placeholders: {
-        meta: `<link rel="icon" href="{file=/assets/icon@64.png}">
-    <link rel="apple-touch-icon" href="{file=/assets/icon@64.png}">
+        meta: `<meta charset="UTF-8">
+
+    <link rel="icon" href="{file=/assets/icon@64.webp}">
+    <link rel="apple-touch-icon" href="{file=/assets/icon@64.webp}">
     <link rel="stylesheet" href="{file=/styles/global.css}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <meta property="og:title" content="{title}">
     <meta property="og:description" content="My website where I put all my projects">
     <meta property="og:determiner" content="">
-    <meta property="og:image" content="{file=/assets/icon@256.png}">
+    <meta property="og:image" content="{file=/assets/icon@256.webp}">
     <meta property="og:type" content="website">
     <meta property="og:url" content="./">
     <meta property="og:locale" content="en">
@@ -16,29 +19,31 @@ module.exports = {
     <meta name="description" content="My website where I put all my projects">
     <meta name="keywords" content="Kale, Ko, Kale Ko, Kale-Ko, Kale_Ko, Coding, Development, Website, Github, Games, Snake, Projects">
     <meta name="robots" content="index, follow">
+    <link rel="canonical" href="{canonical}">
     <meta name="author" content="Kale Ko (https://github.com/Kale-Ko/)">
     <link rel="author" href="https://github.com/Kale-Ko/">
-    <link rel="license" href="https://kaleko.ga/license.txt">
+    <link rel="license" href="{baseUrl}/license.txt">
 
     <script src="{file=/scripts/service-worker-register.js}"></script>
     <script src="{file=/scripts/settings.js}"></script>`,
 
-        offlineMeta: `<link rel="icon" href="{file=image/png;/assets/icon-grey@64.png}">
-        <link rel="apple-touch-icon" href="{file=image/png;/assets/icon-grey@64.png}">
+        offlineMeta: `<meta charset="UTF-8">
+        <link rel="icon" href="{file=image/webp;/assets/icon-grey@64.webp}">
+        <link rel="apple-touch-icon" href="{file=image/webp;/assets/icon-grey@64.webp}">
         <link rel="stylesheet" href="{file=/styles/style=offline.css}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <meta name="robots" content="noindex">
         <meta name="author" content="Kale Ko (https://github.com/Kale-Ko/)">
         <link rel="author" href="https://github.com/Kale-Ko/">
-        <link rel="license" href="https://kaleko.ga/license.txt">`,
+        <link rel="license" href="{baseUrl}/license.txt">`,
 
         nav: `<nav>
-        <div class="dropdown"><img src="{file=/assets/menu@64.png}" alt="Menu Button" width="64" height="64"></img><div class="dropdown-content"></div></div>
-        <div><a href="/" class="image"><img src="{file=/assets/icon@48.png}" width="44" height="44" alt="logo"><p> Home</p></a></div>
-        <div><a href="/snake/">Snake</a></div>
-        <div><a href="/github/">Github</a></div>
-        <div><a href="/settings/">Settings</a></div>
+        <div class="dropdown"><img src="{file=/assets/menu@64.webp}" alt="Menu Button" width="64" height="64"></img><div class="dropdown-content"></div></div>
+        <div><a href="{baseUrl}/" class="image"><img src="{file=/assets/icon@48.webp}" width="44" height="44" alt="logo"><p> Home</p></a></div>
+        <div><a href="{baseUrl}/snake/">Snake</a></div>
+        <div><a href="{baseUrl}/github/">Github</a></div>
+        <div><a href="{baseUrl}/settings/">Settings</a></div>
     </nav>`,
 
         global: `<script src="{file=/scripts/analytics.js}"></script>
