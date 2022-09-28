c4a1fff1f49afb10194a5d94e2b1ff2da02cd876
Kale-Ko <github@kaleko.dev>
2022-09-27 22:57:00 -0400

    Fix alt origins

--- build/builddata.js
+++ build/builddata.js
@@ -37,10 +37,10 @@ module.exports = {
 
         nav: `<nav>
         <div class="dropdown"><img src="{file=/assets/menu@64.webp}" alt="Menu Button" width="64" height="64"></img><div class="dropdown-content"></div></div>
-        <div><a href="{baseUrl}/" class="image"><img src="{file=/assets/icon@48.webp}" width="44" height="44" alt="logo"><p> Home</p></a></div>
-        <div><a href="{baseUrl}/snake/">Snake</a></div>
-        <div><a href="{baseUrl}/github/">Github</a></div>
-        <div><a href="{baseUrl}/settings/">Settings</a></div>
+        <div><a href="/" class="image"><img src="{file=/assets/icon@48.webp}" width="44" height="44" alt="logo"><p> Home</p></a></div>
+        <div><a href="/snake/">Snake</a></div>
+        <div><a href="/github/">Github</a></div>
+        <div><a href="/settings/">Settings</a></div>
     </nav>`,
 
         global: `<script src="{file=/scripts/analytics.js}"></script>
